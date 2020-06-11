a3f5cdca968f13de13b3e41e0b959f57ce9ec9a2
Safi Saqlain <safisaqlain@gmail.com>
2020-06-11 16:35:51 +0500

    checking github actions

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
     <div className={`App ${morning?'morning':'night'}`}>
    <Message count={count}/>
    <h2>Good {morning?'Morning':'Night'}</h2>
-   <button onClick={()=>setMorning(!morning)}>Change Time!</button>
-     <button onClick={()=>setCount(++count)}>Counter Variable!</button>
+   <button onClick={()=>setMorning(!morning)}>Change Time</button>
+     <button onClick={()=>setCount(++count)}>Counter Variable</button>
     </div>
   );
 }
